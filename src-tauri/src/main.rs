5950c39dcf036482ae6e7ea867a2e5aab7f1de6e
codexu <461229187@qq.com>
2024-08-07 10:52:56 +0800

    feat: screenshot

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,32 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-// Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
+use std::process::Command;
+use chrono::{DateTime, Utc};
+
+// 生成文件名
+fn generate_file_name() -> String {
+    let now: DateTime<Utc> = Utc::now();
+    let timestamp = now.timestamp();
+    format!("note_gen_{}.png", timestamp)
+}
+
+// 截图
 #[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}! You've been greeted from Rust!", name)
+fn screenshot() -> String {
+    let screenshot_path = std::env::temp_dir().join(generate_file_name());
+
+    let _ = Command::new("screencapture")
+        .arg("-i")
+        .arg(screenshot_path.to_str().unwrap())
+        .output();
+
+    screenshot_path.to_str().unwrap().to_string()
 }
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![greet])
+        .invoke_handler(tauri::generate_handler![screenshot])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

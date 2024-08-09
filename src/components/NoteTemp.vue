e9baae9ae617a90c142ecb28325ad7265ffb0a26
codexu <461229187@qq.com>
2024-08-09 14:47:27 +0800

    feat: mark badge

--- src/components/NoteTemp.vue
+++ src/components/NoteTemp.vue
@@ -1,33 +1,37 @@
 <template>
-  <section class="w-full bg-white p-4 mb-2" v-for="item in marks" :key="item.id" v-if="marks?.length">
-    <div class="flex w-full border-b border-gray-300">
-      <div class="mr-4 h-40">
-        <a-image :src="item.imgPath" />
-      </div>
-      <div class="w-full flex flex-col justify-between">
-        <div>
-          <a-tag v-for="(keyword, index) in item.keywords" :key="index">{{ keyword }}</a-tag>
-          <p class="line-clamp-3 text-sm leading-6 mt-3 mb-3">{{ item.content }}</p>
+  <template v-for="item in marks" :key="item.id" v-if="marks?.length">
+    <a-badge-ribbon text="Mark">
+      <section class="w-full bg-white p-4 mb-2" >
+        <div class="flex w-full border-b border-gray-300">
+          <div class="mr-4 h-40">
+            <a-image :src="item.imgPath" />
+          </div>
+          <div class="w-full flex flex-col justify-between">
+            <div>
+              <a-tag v-for="(keyword, index) in item.keywords" :key="index">{{ keyword }}</a-tag>
+              <p class="line-clamp-3 text-sm leading-6 mt-3 mb-3">{{ item.content }}</p>
+            </div>
+            <div class="flex justify-between items-end">
+              <a-space>
+                <a-dropdown>
+                  <a-button :icon="h(SwapOutlined)"></a-button>
+                  <template #overlay>
+                    <a-menu>
+                      <a-menu-item v-for="(tag, index) in tags" :key="index">
+                        <a @click="changeTag(item, tag)">{{ tag.name }}</a>
+                      </a-menu-item>
+                    </a-menu>
+                  </template>
+                </a-dropdown>
+                <a-button danger @click="deleteMark(item)" :icon="h(DeleteOutlined)"></a-button>
+              </a-space>
+              <span class="mb-0 text-sm text-gray-400">{{ timeAgo(item.createdAt) }}</span>
+            </div>
+          </div>
         </div>
-        <div class="flex justify-between items-end">
-          <a-space>
-            <a-dropdown>
-              <a-button :icon="h(SwapOutlined)"></a-button>
-              <template #overlay>
-                <a-menu>
-                  <a-menu-item v-for="(tag, index) in tags" :key="index">
-                    <a @click="changeTag(item, tag)">{{ tag.name }}</a>
-                  </a-menu-item>
-                </a-menu>
-              </template>
-            </a-dropdown>
-            <a-button danger @click="deleteMark(item)" :icon="h(DeleteOutlined)"></a-button>
-          </a-space>
-          <span class="mb-0 text-sm text-gray-400">{{ timeAgo(item.createdAt) }}</span>
-        </div>
-      </div>
-    </div>
-  </section>
+      </section>
+    </a-badge-ribbon>
+  </template>
   <div v-else class="w-full empty-wrap flex justify-center items-center">
     <a-empty description="你还没有任何记录" />
   </div>

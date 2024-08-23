e058acdd7809bd204c9279befb7a384df34f8411
codexu <461229187@qq.com>
2024-08-23 16:37:27 +0800

    feat: 使用 xcap 代替 screenshots

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,45 +1,20 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod screenshot;
+use screenshot::screenshot_path;
+mod keyword;
+use keyword::cut_words;
+
 use tauri::{CustomMenuItem, Manager, SystemTray, SystemTrayEvent};
 use tauri::SystemTrayMenu;
 
-// 截图
-use base64::{engine::general_purpose, Engine as _};
-use screenshots::Screen;
-
-#[tauri::command]
-fn screenshot_base64() -> String {
-    let screen = Screen::from_point(100, 100).unwrap();
-    let image = screen
-        .capture().unwrap();
-    let buffer = image.buffer();
-    let base64_str = general_purpose::STANDARD_NO_PAD.encode(buffer);
-    base64_str
-}
-
-// 关键词提取
-use jieba_rs::Jieba;
-use jieba_rs::{TfIdf, KeywordExtract};
-#[tauri::command]
-fn cut_words(str: String) -> Vec<String> {
-    let jieba = Jieba::new();
-    let keyword_extractor = TfIdf::default();
-    let top_key = keyword_extractor.extract_keywords(
-        &jieba,
-        &str,
-        5,
-        vec![],
-    );
-    top_key.iter().map(|x| x.keyword.clone()).collect()
-}
-
 fn main() {
-    let screenshot = CustomMenuItem::new("screenshot".to_string(), "记录");
-    let quit = CustomMenuItem::new("quit".to_string(), "退出");
+    let screenshot_item = CustomMenuItem::new("screenshot".to_string(), "记录");
+    let quit_item = CustomMenuItem::new("quit".to_string(), "退出");
     let tray_menu = SystemTrayMenu::new()
-        .add_item(screenshot)
-        .add_item(quit);
+        .add_item(screenshot_item)
+        .add_item(quit_item);
    
     let tray = SystemTray::new().with_menu(tray_menu);
     tauri::Builder::default()
@@ -58,7 +33,7 @@ fn main() {
               }
             _ => {}
         })
-        .invoke_handler(tauri::generate_handler![screenshot_base64, cut_words])
+        .invoke_handler(tauri::generate_handler![screenshot_path, cut_words])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }

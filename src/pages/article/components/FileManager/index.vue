b1843c140e1c1f21e1aee2ab6eeee1c2ea8517cc
codexu <461229187@qq.com>
2024-09-05 08:59:32 +0800

    feat: 打开文件目录

--- src/pages/article/components/FileManager/index.vue
+++ src/pages/article/components/FileManager/index.vue
@@ -3,7 +3,7 @@
     <div class="flex justify-rounded h-12 items-center bg-gray-100 border-b-thin overflow-hidden relative">
       <FolderAdd />
       <v-icon size="small" class="flex-1 border-e-thin" v-tooltip="'搜索'">mdi-folder-search-outline</v-icon>
-      <v-icon size="small" class="flex-1" v-tooltip="'打开文件夹'">mdi-folder-open-outline</v-icon>
+      <v-icon size="small" class="flex-1" v-tooltip="'打开文件夹'" @click="openFolder">mdi-folder-open-outline</v-icon>
     </div>
     <FileTree />
   </div>
@@ -12,8 +12,12 @@
 <script lang="ts" setup>
 import FileTree from './FileTree.vue';
 import FolderAdd from './FolderAdd.vue';
+import { open } from '@tauri-apps/api/shell';
+import { appDataDir } from '@tauri-apps/api/path';
+
+async function openFolder() {
+  const dir = await appDataDir();
+  console.log(dir);
+  await open(`${dir}article`);
+}
 </script>
-
-<style lang="scss" scoped>
-
-</style>

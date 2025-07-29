7727c616a4665da237e708a9ff05886da84e1f35
wuxufanhua <89204585@qq.com>
2025-07-29 12:35:48 +0800

    fix(#444)分屏预览情况在,本地图片粘贴后,无法立即加载的问题.

--- src/app/core/article/md-editor.tsx
+++ src/app/core/article/md-editor.tsx
@@ -124,7 +124,10 @@ export function MdEditor() {
       input: (value) => {
         saveCurrentArticle(value)
         emitter.emit('editor-input')
-        handleLocalImage(vditor)
+        setTimeout(() => {
+          handleLocalImage(vditor)
+        }, 1100)
+
       },
       mode: localMode,
       upload: {
@@ -193,7 +196,7 @@ export function MdEditor() {
   // 处理本地相对路径图片
   async function handleLocalImage(vditor: Vditor) {
     const workspace = await getWorkspacePath()
-    const previews = [vditor.vditor.ir?.element, vditor.vditor.sv?.element, vditor.vditor.wysiwyg?.element]
+    const previews = [vditor.vditor.ir?.element, vditor.vditor.sv?.element, vditor.vditor.wysiwyg?.element,vditor.vditor.preview?.element]
     previews.forEach(element => {
       element?.querySelectorAll('img').forEach(async (img) => {
         let src = img.getAttribute('src')
